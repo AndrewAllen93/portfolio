4b95d899f46925dc6f5bc9f9059c659f88a9e3f4
Andrew Allen <allenkandrew@gmail.com>
2024-07-08 14:22:02 -0700

    changes

--- main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.toggle-menu').addEventListener('click', function() {
-        this.classList.toggle('active');
-        document.getElementById('menu').classList.toggle('open');
-    });
-});
